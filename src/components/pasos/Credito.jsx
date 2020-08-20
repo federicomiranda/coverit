83cef4f376c84c0731ec238074159a8af297e041
federicomiranda <federico.mirandaa@gmail.com>
2020-08-20 02:06:16 -0300

    varios

--- src/components/pasos/Credito.jsx
+++ src/components/pasos/Credito.jsx
@@ -49,7 +49,7 @@ const Credito = () => {
 
   const handleContinue = () => {
     fetch(
-      `${BASE_URL}/solicitar-emision?cotizacion_id=${coberturaSeleccionada.id}&tipo_documento=${cliente.dniElegido}&documento=${cliente.dniValue}&sexo=${cliente.sexo}&cuit=${cliente.CUILT}&situacion_afip=${cliente.condicionIVA}&iibb=${cliente.condicionIIBB}&patente=${dataVehiculo.patente}&chasis=${dataVehiculo.chasis}&motor=${dataVehiculo.motor}&anios_siniestros=0&calle=${cliente.calle}&numero=${cliente.nro}&vigencia_desde=2020-09-01&numero_tarjeta=${nroTarjeta}&telefono=${cliente.tel}&estado_civil=1`,
+      `${BASE_URL}/solicitar-emision?cotizacion_id=${coberturaSeleccionada.id}&tipo_documento=${cliente.dniElegido}&documento=${cliente.dniValue}&sexo=${cliente.sexo}&cuit=${cliente.CUILT}&situacion_afip=${cliente.condicionIVA}&iibb=${cliente.condicionIIBB}&patente=${dataVehiculo.patente}&chasis=${dataVehiculo.chasis}&motor=${dataVehiculo.motor}&anios_siniestros=0&calle=${cliente.calle}&numero=${cliente.nro}&vigencia_desde=2020-09-01&numero_tarjeta=${nroTarjeta}&telefono=${cliente.tel}&estado_civil=1&forma_pago=4`,
       {
         method: 'POST',
         headers: {
